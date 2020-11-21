016db932433234c3a5fca6457a65cb7cbb2e54ab
Christopher Shoo <rishaelibenson@gmail.com>
2020-11-21 17:54:01 +0300

    Fix password validation erors (incorect regex) Remove api keys from .env.example file

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -13,7 +13,6 @@ trait PasswordValidationRules
      */
     protected function passwordRules()
     {
-        return ['required', 'string', new Password, 'confirmed', 'regex:/^(?=.\d)(?=.[a-z])(?=.*[A-Z]).{6,20}$/'];
+        return ['required', 'string', new Password, 'confirmed', 'regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/'];
     }
-    //regex:/^(?=.\d)(?=.[a-z])(?=.*[A-Z]).{6,20}$/
 }
