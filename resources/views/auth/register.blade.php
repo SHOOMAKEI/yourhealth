c7579c6423f949ca0a3d334e5005113f4d744a25
Christopher Shoo <rishaelibenson@gmail.com>
2020-11-21 21:15:36 +0300

    Refactor registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,9 +10,9 @@
                             <div class="text-center w-75 m-auto pb-2">
                                 <h4 class="text-dark-50 text-center mt-0 font-weight-bold">Create Account</h4>
                             </div>
-                            <form method="POST" action="{{route('register')}}">
+                            <form method="POST" action="{{route('register')}}" class="needs-validation" novalidate>
                                 @csrf
-                                <div class="form-group">
+                                <div class="form-group position-relative mb-3">
                                     <label for="fullname">{{__('auth.register.service_provider.full_name')}}</label>
                                     <input 
                                         type="text"
@@ -21,16 +21,22 @@
                                         name="name"
                                         required=""
                                         placeholder="{{__('auth.register.service_provider.full_name')}}">
+                                    <div class="valid-tooltip">
+                                        Looks good!
+                                    </div>
+                                    <div class="invalid-tooltip">
+                                        Please enter full name.
+                                    </div>
                                     @error('name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group position-relative mb-3">
                                     <input type="text" hidden name="category" value="service-provider" required="">
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group position-relative mb-3">
                                     <label for="email">{{__('auth.register.service_provider.email')}}</label>
                                     <input
                                         type="email"
@@ -39,13 +45,19 @@
                                         name="email"
                                         required=""
                                         placeholder="{{__('auth.register.service_provider.email')}}">
+                                    <div class="valid-tooltip">
+                                        Looks good!
+                                    </div>
+                                    <div class="invalid-tooltip">
+                                        Please provide an email.
+                                    </div>
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group position-relative mb-3">
                                     <label for="mobile_number">{{__('auth.register.service_provider.mobile_number')}}</label>
                                     <input
                                         type="text"
@@ -53,20 +65,40 @@
                                         id="mobile_number"
                                         name="mobile_number"
                                         required=""
+                                        data-toggle="input-mask"
+                                        data-mask-format="+255 000 000 000"
                                         placeholder="{{__('auth.register.service_provider.mobile_number')}}">
+                                    <div class="valid-tooltip">
+                                        Looks good!
+                                    </div>
+                                    <div class="invalid-tooltip">
+                                        Please provide a phone number
+                                    </div>
                                     @error('mobile_number')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group position-relative mb-3">
                                     <label for="password1">{{__('auth.register.service_provider.password')}}</label>
                                     <input type="password" id="password1" name="password" class="form-control" required="">
+                                    <div class="valid-tooltip">
+                                        Looks good!
+                                    </div>
+                                    <div class="invalid-tooltip">
+                                        Please provide a password.
+                                    </div>
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group position-relative mb-3">
                                     <label for="password2">{{__('auth.register.service_provider.password_confrimation')}}</label>
                                     <input type="password" id="password2" name="password_confirmation" class="form-control" required="">
+                                    <div class="valid-tooltip">
+                                        Looks good!
+                                    </div>
+                                    <div class="invalid-tooltip">
+                                        Please confirm a password.
+                                    </div>
                                 </div>
                                 <div class="form-group mb-0 text-center">
                                     <button class="btn btn-primary btn-block" type="submit"> Sign Up </button>
