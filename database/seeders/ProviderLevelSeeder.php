40a0270b9c4845189f430811caa3cdac4adecf75
Michael Omakei <omakei96@gmail.com>
2021-01-11 20:11:22 +0300

    WIP

---
+++ database/seeders/ProviderLevelSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ProviderLevel;
+use FontLib\Table\Type\name;
+use Illuminate\Database\Seeder;
+
+class ProviderLevelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+        ProviderLevel::create(['name' => 'Health Post']);
+    }
+}
